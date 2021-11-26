3e9743d97d256214ea109ecb3fbe5d70fbe290c9
motmarsda <emma.lindell.6567@student.uu.se>
2021-11-26 11:46:14 +0100

    done with start page layout

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,28 +1,34 @@
 <template>
   <body>
-    <!-- <img src=https://ae01.alicdn.com/kf/H4f714299b1b8412f8e1548736541f7afd/7x5FT-Retro-Vintage-Carnival-Circus-Spotlight-Circo-Stage-Custom-Photo-Studio-Backdrop-Background-Vinyl-220cm-X.jpg alt="id">
-   -->
     <div id="nav">
       <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
     </div>
-     
-     <div class="wrapper"> 
+    <header>
+      <h1>Quizcus</h1>
+    </header>
+     <div class="wrapper">
+        <!--Create Poll button-->
+        <router-link v-bind:to="'/create/'+lang">
+            <button>{{uiLabels.createPoll}}</button>
+        </router-link>
 
-      <div id="createPoll">
-          <router-link v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link>
-      </div>
-      <div id="titleBox">
-      <h6> QUIZCUZ</h6>
-      </div>
-      <div id="partPoll">
-      <label>
-        Write poll id: 
-        <input type="text" v-model="id">
-      </label>
-      <router-link v-bind:to="'/poll/'+id" tag="button"><br>{{uiLabels.participatePoll}}</router-link>
-      </div>
-      </div>
-    
+        <!--Participate in Poll button-->
+        <!--Skapa en ny vy flr sidan vi hamnar på där man får skriva in pollid för att komma till "'/poll/'+id" -->
+        <router-link v-bind:to="'/poll/'+id">
+            <button>{{uiLabels.participatePoll}}</button>
+        </router-link>
+
+        <!-- ORGINALKOD TA INTE BORT FÖR FAN div
+          <div id="partPoll">
+            <label>
+              Write poll id:
+              <input type="text" v-model="id">
+            </label>
+            <router-link v-bind:to="'/poll/+id'" tag="button"><br>{{uiLabels.participatePoll}}</router-link>
+            </div>
+          </div-->
+
+        </div>
   </body>
 </template>
 
@@ -61,41 +67,41 @@ template{
   margin: 0px;
 }
 body{
-  background-image: url(https://ae01.alicdn.com/kf/H4f714299b1b8412f8e1548736541f7afd/7x5FT-Retro-Vintage-Carnival-Circus-Spotlight-Circo-Stage-Custom-Photo-Studio-Backdrop-Background-Vinyl-220cm-X.jpg);
+  background-image: url(https://png.pngtree.com/thumb_back/fw800/background/20200916/pngtree-circus-background-image_398762.jpg);
   background-repeat: no-repeat;
   background-size:cover;
-  background-position: center;
+  background-position: bottom;
   background-attachment: fixed;
   margin: 0px;
   padding: 0px;
-  border: 1px solid rgb(0, 0, 0);
-  height: 1320px;
 }
+
+button{
+    border-radius: 8px;
+}
+
 .wrapper {
    margin: 0px;
    width: 95%;
    height: 95%;
-   padding-left: 5%;
    display: grid;
    grid-gap: 5px;
-   grid-template-columns: 33% 33% 33%;
+   grid-template-columns: 50% 50%;
    align-items: center;
   }
-  .wrapper button{
-    height: 5%;
-    width: 80%;
-  }
 
-#createPoll{
-   border: 1px solid rgb(0, 0, 0);
-   width: 50%;
-   height: 20%;
-   background-color: wheat;
-   grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 2;
-    grid-row-end: 3;
+.wrapper button{
+  width: 50%;
+  height: 50px;
+  background-color: wheat;
+  text-transform: uppercase;
 }
+
+button:hover {
+  cursor: pointer;
+}
+
+
 #nav button{
   width: 150px;
   height: 50px;
@@ -104,33 +110,15 @@ body{
   background-color: wheat;
   position: absolute;
 }
-  
-/*#createPoll:hover{
-   box-shadow: 14px 14px 9px black;
-   transform : scale(1.06, 1.06);
-   transition: all 0.2s ease-in-out;
-}*/
-h6{
-  font-size: 70px;
-}
-#titleBox{
-  grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 2;
-    grid-row-end: 3;
-}
-#partPoll{
-  grid-column-start: 3;
-    grid-column-end: 4;
-    grid-row-start: 2;
-    grid-row-end: 3;
-   border: 1px solid rgb(0, 0, 0);
-   width: 50%;
-   height: 20%;
-   /*position: absolute;/*/
-   background-color: wheat;
-   align-content: center;
 
+header {
+  font-size: 50pt;
+  text-align: center;
+  padding-top: 50px;
+  margin: 0;
+  color: white;
+  text-shadow: 3px 3px #990000;
 }
+
 
 </style>
