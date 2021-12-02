4a6c3a181b6ac46ef98583a0fc9c91e1e76f3fa9
kerstinwarja <kerwarja@gmail.com>
2021-12-02 11:28:31 +0100

    kladd

--- src/views/PollLibrary.vue
+++ src/views/PollLibrary.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-  <header>
-    Yoyoyo
-  </header>
-  </div>
+  <body>
+   <div id="partPoll">
+            <label>
+              Write poll id:
+              <input type="text" v-model="id">
+            </label>
+            <router-link v-bind:to="'/poll/+id'" tag="button"><br>{{uiLabels.participatePoll}}hej</router-link>
+            </div>
+  </body>
 </template>
+<script>
+  export default {
+    name: 'PollLibrary',
+    data: function () {
+      return {
+        uiLabels:{},
+        id: "",
+        lang: "en"
+      }
+    }
+  }
+</script>
+<style>
+
+  #partPoll{
+    background-color: wheat;
+    width: 30%
+  }
+
+</style>
