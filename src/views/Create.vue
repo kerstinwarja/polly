0ae54e2547ba9e07bbec74b127c882517b58ee48
HannahLiljestam <93650279+HannahLiljestam@users.noreply.github.com>
2021-12-07 13:35:30 +0100

    CSS changes to PollLibrary.vue and Start.vue

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,12 +1,48 @@
 <template>
+<body>
+
+<header>
+  <h1>Add questions and answers to your quiz!</h1>
+</header>
+
+
+<div class="wrap2">
+<div id="preview">
+  <div id="previewTitle">
+    <p id="as">Preview</p>
+  </div>
+</div>
+
+<div class="createWindow">
+  <h3> {{uiLabels.question}}: </h3> <br>
+  <textarea id="quizTitle" type="text" v-model="question" placeholder="Write your question ..."></textarea> <br>
+  <h3>Answer:</h3> <br>
+  <textarea id="desIptBox" type="text" v-for="(_, i) in answers" v-model="answers[i]" v-bind:key="'answer'+i" placeholder="Add an answer ..."></textarea>
+  <button id="addQuestionButton" v-on:click="addAnswer">
+    Add answer
+  </button>
+
+  <button type="submit" id="updatePre" v-on:click="updatePreview()">
+    Update preview
+  </button>
+</div>
+
+
+</div>
+
   <div>
+    <!--
     Poll link:
     <input type="text" v-model="pollId">
     <button v-on:click="createPoll">
       Create poll
     </button>
-    <div>
-      {{uiLabels.question}}:
+    -->
+
+
+<!--    <div>
+
+      <h3> {{uiLabels.question}}: </h3>
       <input type="text" v-model="question">
       <div>
         Answers:
@@ -17,7 +53,8 @@
           Add answer alternative
         </button>
       </div>
-    </div>
+    </div>-->
+
     <button v-on:click="addQuestion">
       Add question
     </button>
@@ -28,6 +65,8 @@
     {{data}}
     <router-link v-bind:to="'/result/'+pollId">Check result</router-link>
   </div>
+
+</body>
 </template>
 
 <script>
@@ -71,7 +110,85 @@ export default {
     },
     runQuestion: function () {
       socket.emit("runQuestion", {pollId: this.pollId, questionNumber: this.questionNumber})
+    },
+    updatePreview(){
+      //THis is the code fro updating title and description
+      var c1 = document.getElementById('quizTitle').value;
+      var d1 = document.getElementById('as');
+      d1.innerHTML = c1;
+      var c2 = document.getElementById('desIptBox').value;
+      var d2 = document.getElementById('pdes');
+      d2.innerHTML = c2;
+      document.getElementById("previewPic").style.visibility= "visible";
     }
   }
 }
 </script>
+
+
+<style>
+
+header {
+  font-size: 20pt;
+  text-shadow: 3px 3px navy;
+}
+
+h3{
+  margin:0px;
+  padding: 2% 10% 1%;
+  text-align: left;
+  color: Navy;
+}
+
+.createWindow{
+  background-color: wheat;
+  width: 100%;
+}
+
+.wrap2 {
+  margin: 0px;
+  padding-left: 4%;
+  width: 95%;
+  height: 95%;
+  display: grid;
+  grid-gap: 5%;
+  grid-template-columns: 61% 31%;
+  align-items: center;
+}
+
+body textarea{
+  width: 80%;
+  background-color: wheat;
+  color: Navy;
+  resize:none;
+  padding: 5px 5px 5px;
+  font-family: sans-serif;
+  border: 2px solid;
+}
+
+#preview{
+  background-color: black;
+  background-size: cover;
+  max-height: 100%;
+  background-position: bottom;
+  color: Grey;
+  height: 100%;
+  border: 5px black solid;
+}
+
+#previewTitle{
+  font-size: 30px;
+  text-shadow: 3px 3px navy;
+  color: white;
+  height: 15%;
+  line-break: auto;
+  max-height: 15%;
+}
+
+#addQuestionButton {
+  height: 10%;
+  width: 20%;
+  background-color: #ffcc00;
+}
+
+</style>
